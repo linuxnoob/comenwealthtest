043954a835f93317377582f254abd7c6fd5cfa10
linuxnoob <blackprimenoz@gmail.com>
2020-11-29 07:24:23 +0700

    Add files via upload

---
+++ src/test/java/com/example/testCommonwealth/TestCommonwealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testCommonwealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCommonwealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
