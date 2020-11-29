96689633edf887b3e50d943c2189079b66970d95
garinda.burnama <blackprimenoz@gmail.com>
2020-11-29 07:57:19 +0700

    Master

---
+++ src/main/java/com/testCommonwealth/testCommon/TestCommonApplication.java
@@ -0,0 +1,13 @@
+package com.testCommonwealth.testCommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testCommonwealth/testCommon/TestCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testCommonwealth.testCommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
